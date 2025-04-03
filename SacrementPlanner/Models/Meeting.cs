4e76210bcea1052ad23fa4cfd579db6edcd42cae
Ben Davis <bendavis493@gmail.com>
2025-04-02 20:43:34 -0600

    Created Models

---
+++ SacrementPlanner/Models/Meeting.cs
@@ -0,0 +1,16 @@
+﻿namespace SacrementPlanner.Models
+{
+    public class Meeting
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Conducting { get; set; }
+        public string OpeningHymn {  get; set; }
+        public string ClosingHymn { get; set; }
+        public string SacramentHymn { get; set; }
+        public string? IntermediateNumber { get; set; }
+        public string OpeningPrayer {  get; set; }
+        public string ClosingPrayer { get; set; }
+        public ICollection<Speaker> Speakers { get; set; }
+    }
+}
---
+++ SacrementPlanner/Models/Speaker.cs
@@ -0,0 +1,10 @@
+﻿namespace SacrementPlanner.Models
+{
+    public class Speaker
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Subject { get; set; }
+        public int MeetingId { get; set; }
+    }
+}
